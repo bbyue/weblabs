53972c78c3937c7548a9145bf918c3613e7ccaa6
Xenia Yakimova <6ki6mov6a@gmail.com>
2025-02-28 11:59:25 +0400

    feat:LAB1-5 Добавлены улучшения по вариантам (введение CORS-ограничений)

--- api/eventAPI.js
+++ api/eventAPI.js
@@ -2,6 +2,28 @@ const express = require('express');
 const { Event } = require('../models/event');
 const router = express.Router();
 const { Op } = require('sequelize');
+// проверка на origin
+const checkTrustedOrigin = (req, res, next) => {
+    const origin = req.get('origin'); // заголовок Origin
+  
+    // разрешение запроса если origin отсутствует
+    if (!origin) {
+      console.warn('заголовок Origin отсутствует. запрос разрешён.');
+      return next();
+    }
+  
+    // получение списка доверенных origin из .env
+    const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS.split(',');
+  
+    // проверкана доверенность
+    if (allowedOrigins.includes(origin)) {
+      console.log(`запрос от доверенного origin: ${origin}`);
+      next();
+    } else {
+      console.warn(`запрос от недоверенного origin: ${origin}`);
+      res.status(403).json({ error: 'запрос запрещён' });
+    }
+  };
 
 /**
  * @swagger
--- index.js
+++ index.js
@@ -8,7 +8,32 @@ app.use(express.json());
 app.use(cors()); 
 
 const PORT = process.env.PORT; 
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
 
+const corsOptions = {
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); 
+  
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true); 
+      } else {
+        console.warn(`Warning: Request from origin "${origin}" is not allowed by CORS policy.`);
+        callback(new Error('Not allowed by CORS'), false); 
+      }
+    },
+    methods: ['GET', 'POST'], 
+    optionsSuccessStatus: 200
+  };
+  
+  
+  app.use(cors(corsOptions));
+  app.use((err, req, res, next) => {
+    if (err.message === 'Not allowed by CORS') {
+      return res.status(405).json({ message: 'Method Not Allowed: Origin not permitted by CORS policy.' });
+    }
+    next(err); 
+  });
+app.use(cors(corsOptions));
 app.get('/', (req, res) => {
     res.json({ message: '123' });
 });
